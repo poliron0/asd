9352c742ce93387de24559e72471c3eab698685c
sr <sr@rs.com>
2017-11-06 09:07:58 +0200

    Server now sends index.html in case no other route found

--- server/app.ts
+++ server/app.ts
@@ -2,8 +2,14 @@ import * as express from 'express'
 import * as path from 'path'
 
 let app = express()
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath))
 
-app.use(express.static(path.join(__dirname, '../public')))
+//In case no other route found return 
+//index.html(for react routing to handle)
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(publicPath, '/index.html'))
+})
 
 app.listen(3000, (err) => {
     if(err) {
@@ -11,3 +17,4 @@ app.listen(3000, (err) => {
     }
     console.log("Listening on port 3000")
 })
+
